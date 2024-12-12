0e4e6d2eb1bd63ecd8e2dfe1d796bb8ab823ed6d
bpedrazaa <bpedrazaa@gmail.com>
2024-12-12 11:19:43 -0400

    Handle multistream better and catch multiple exceptions

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import asyncio
+import base64
 import cv2
+import json
 import logging
 import os
-import signal
 import time
 import websockets
 
@@ -44,48 +45,66 @@ async def stream_video(websocket, rtsp_url):
                 if not ret:
                     break  # End of stream
 
-                # Encode
-                _, jpeg_frame = cv2.imencode(".jpg", frame)
-                jpeg_data = jpeg_frame.tobytes()
+                # Resize and encode the frame as JPEG
+                # target_size = (640, 360)
+                # frame = cv2.resize(frame, target_size)
+
+                # Process frame
+                _, buffer = cv2.imencode(".jpg", frame)
+                frame_data = base64.b64encode(buffer).decode("utf-8")
+
+                # Prepare the json message
+                message = {"frame": frame_data}
 
                 # Send the JPEG frame over WebSocket
-                await websocket.send(jpeg_data)
+                await websocket.send(json.dumps(message))
 
                 await asyncio.sleep(
                     frame_rate_target
-                )  # Sleep to match the target frame rate
+                )  # Sleep to match the target frame rate1
 
         except Exception as e:
             logging.error(f"Error: {e}")
+
         finally:
             cap.release()
 
 
-# WebSocket server to handle connections and route to the correct path handler
-async def main():
-    print(stream_urls)
-
-    async def handler(websocket):
+async def handler(websocket):
+    try:
         if stream_urls:
             path = websocket.request.path
-            rtsp_url = stream_urls.get(path)
-            await stream_video(websocket, rtsp_url)
-        else:
-            await websocket.close()
+            if path in (stream_urls.keys()):
+                rtsp_url = stream_urls.get(path)
+                await stream_video(websocket, rtsp_url)
 
+    except websockets.exceptions.ConnectionClosedError as e:
+        logging.error("Connection Closed Error: ", e)
+
+    except websockets.exceptions.ProtocolError as e:
+        logging.error("Protocol Error: ", e)
+
+    except websockets.exceptions.InvalidHandshake as e:
+        logging.error("Invalid Handshake:", e)
+
+    except EOFError as e:
+        logging.error("EOF Error: ", e)
+
+    finally:
+        await websocket.close()
+
+
+# WebSocket server to handle connections and route to the correct path handler
+async def main():
+    print(f"Available stream paths: {' | '.join(stream_urls.keys())}")
     # Start the WebSocket server
-    async with websockets.serve(handler, HOST, PORT, compression="deflate"):
-        logging.info(f"WebSocket server listening on ws://{HOST}:{PORT}")
+    async with websockets.serve(
+        handler, HOST, PORT, compression="deflate", ping_interval=None
+    ):
+        logging.info("WebSocket server initialized")
         await asyncio.Future()  # run forever
-
-
-def shutdown_handler(signum, frame):
-    logging.info("Shutting down gracefully...")
-    # Cancel any active tasks and clean up resources
-    asyncio.get_event_loop().stop()
 
 
 # Run the WebSocket server
 if __name__ == "__main__":
     asyncio.run(main())
-    signal.signal(signal.SIGINT, shutdown_handler)
